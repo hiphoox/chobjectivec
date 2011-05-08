02d06cc11124eaadfdbf1d5baf03ddc15b79d235
Norberto Ortigoza <norberto.ortigoza@gmail.com>
2011-05-07 23:51:13 -0500

    Initial Commit

---
+++ CHObjectiveC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CHObjectiveC
+//
+//  Created by Norberto Ortigoza on 07/05/11.
+//  Copyright 2011 Raku Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+  // insert code here...
+  NSLog(@"Hello, World!");
+
+  [pool drain];
+    return 0;
+}
+
